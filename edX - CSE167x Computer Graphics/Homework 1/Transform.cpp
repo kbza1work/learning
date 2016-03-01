925b864389f2094a4c06f172521a9fb022d181e9
Brent Houghton <brent@slixbits.com>
2016-03-01 12:02:55 -0800

    Finish homework 1 from edX course CSE167x
    Homework 1 involves implementing basic OpenGL functionality for a camera
    that rotates around a teapot at a fixed distance.

---
+++ edX - CSE167x Computer Graphics/Homework 1/Transform.cpp
@@ -0,0 +1,68 @@
+// Transform.cpp: implementation of the Transform class.
+
+#include "Transform.h"
+
+//Please implement the following functions:
+
+// Helper rotation function.  
+mat3 Transform::rotate(const float degrees, const vec3& axis) {
+  const float radians = glm::radians(degrees);
+  const mat3 I(1.0);
+
+  const mat3 term1 = cos(radians) * I;
+  const mat3 term2 = (1 - cos(radians)) * mat3(
+    axis.x * axis.x, axis.x * axis.y, axis.x * axis.z,
+    axis.x * axis.y, axis.y * axis.y, axis.y * axis.z,
+    axis.x * axis.z, axis.y * axis.x, axis.z * axis.z
+  );
+  const mat3 term3 = sin(radians) * mat3(
+    0.0, axis.z, -1.0 * axis.y,
+    -1.0 * axis.z, 0.0, axis.x,
+    axis.y, -1.0 * axis.x, 0.0
+  );
+
+  return term1 + term2 + term3;
+}
+
+// Transforms the camera left around the "crystal ball" interface
+void Transform::left(float degrees, vec3& eye, vec3& up) {
+  const vec3 rotationAxis = vec3(0.0, 1.0, 0.0);
+  eye = Transform::rotate(degrees, rotationAxis) * eye;
+}
+
+// Transforms the camera up around the "crystal ball" interface
+void Transform::up(float degrees, vec3& eye, vec3& up) {
+  const vec3 rotationAxis = vec3(-1.0, 0.0, 0.0);
+  eye = Transform::rotate(degrees, rotationAxis) * eye;
+  up = Transform::rotate(degrees, rotationAxis) * up;
+}
+
+mat4 Transform::lookAt(vec3 eye, vec3 up) {
+  const vec3 center = vec3(0.0, 0.0, 0.0);
+  const vec3 f = glm::normalize(vec3(
+    center.x - eye.x, center.y - eye.y, center.z - eye.z
+  ));
+  const vec3 s = glm::cross(f, glm::normalize(up));
+  const vec3 u = glm::cross(glm::normalize(s), f);
+
+  const vec4 column0 = vec4(s.x, u.x, -1.0 * f.x, 0.0);
+  const vec4 column1 = vec4(s.y, u.y, -1.0 * f.y, 0.0);
+  const vec4 column2 = vec4(s.z, u.z, -1.0 * f.z, 0.0);
+  const vec4 column3 = vec4(
+    -s.x * eye.x - s.y * eye.y - s.z * eye.z,
+    -u.x * eye.x - u.y * eye.y - u.z * eye.z,
+    f.x * eye.x + f.y * eye.y + f.z * eye.z, 1.0
+  );
+
+  return mat4(column0, column1, column2, column3);
+}
+
+Transform::Transform()
+{
+
+}
+
+Transform::~Transform()
+{
+
+}
---
+++ edX - CSE167x Computer Graphics/Homework 1/unitTests.cpp
@@ -0,0 +1,141 @@
+#include <cmath>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <glm/glm.hpp>
+
+#include "Transform.h"
+
+const float EPSILON = 0.1;
+
+std::string format(const vec3& vector) {
+	std::ostringstream text;
+	text << "(" << vector.x << ", " << vector.y << ", " << vector.z << ")";
+	return text.str();
+}
+
+std::string format(const vec4& vector) {
+	std::ostringstream text;
+	text << "(" << vector[0] << ", " << vector[1] << ", " << vector[2] << ", " << vector[3] << ")";
+	return text.str();
+}
+
+std::string format(const mat4& matrix) {
+	std::ostringstream text;
+	text << "[" << format(matrix[0]) << "]\n"
+		<< "[" << format(matrix[1]) << "]\n"
+		<< "[" << format(matrix[2]) << "]\n"
+		<< "[" << format(matrix[3]) << "]" << std::endl;
+	return text.str();
+}
+
+bool eq(const float a, const float b, const float epsilon) {
+	return abs(a - b) < epsilon;
+}
+
+bool eq(const vec3& a, const vec3& b, const float epsilon) {
+	return (
+		eq(a.x, b.x, epsilon) &&
+		eq(a.y, b.y, epsilon) &&
+		eq(a.z, b.z, epsilon)
+	);
+}
+
+bool eq(const vec4& a, const vec4& b, const float epsilon) {
+	return (
+		eq(a[0], b[0], epsilon) &&
+		eq(a[1], b[1], epsilon) &&
+		eq(a[2], b[2], epsilon) &&
+		eq(a[3], b[3], epsilon)
+	);
+}
+
+bool eq(const mat4& a, const mat4& b, const float epsilon) {
+	return (
+		eq(a[0], b[0], epsilon) &&
+		eq(a[1], b[1], epsilon) &&
+		eq(a[2], b[2], epsilon) &&
+		eq(a[3], b[3], epsilon)
+	);
+}
+
+bool testAssert(const vec3& expected, const vec3& actual, const std::string& message) {
+	if(eq(expected, actual, EPSILON)) {
+		std::cout << "\tPASSED" << std::endl;
+		return true;
+	} else {
+		std::cout << "FAILED (" << message << ")\n\texpected: ";
+		std::cout << format(expected) << "; actual: " << format(actual) << std::endl;
+		return false;
+	}
+}
+
+bool testAssert(const mat4& expected, const mat4& actual, const std::string& message) {
+	if(eq(expected, actual, EPSILON)) {
+		std::cout << "\tPASSED" << std::endl;
+		return true;
+	} else {
+		std::cout << "FAILED (" << message << ")"
+			<< "\nexpected:\n" << format(expected)
+			<< "\nactual:\n" << format(actual)
+			<< std::endl;
+		return false;
+	}
+}
+
+void leftByPositiveAngle() {
+	std::cout << "leftByPositiveAngle:" << std::endl;
+	const float angle = 90.0;
+	vec3 eye = vec3(0.0, 0.0, 5.0);
+	vec3 up = vec3(0.0, 1.0, 0.0);
+	Transform::left(angle, eye, up);
+	testAssert(vec3(5.0, 0.0, 0.0), eye, "eye coordinates incorrect");
+	testAssert(vec3(0.0, 1.0, 0.0), up, "up coordinates incorrect");
+}
+
+void leftByNegativeAngle() {
+	std::cout << "leftByNegativeAngle:" << std::endl;
+	const float angle = -90.0;
+	vec3 eye = vec3(0.0, 0.0, 5.0);
+	vec3 up = vec3(0.0, 1.0, 0.0);
+	Transform::left(angle, eye, up);
+	testAssert(vec3(-5.0, 0.0, 0.0), eye, "eye coordinates incorrect");
+	testAssert(vec3(0.0, 1.0, 0.0), up, "up coordinates incorrect");
+}
+
+void upByPositiveAngle() {
+	std::cout << "upByPositiveAngle:" << std::endl;
+	const float angle = 90.0;
+	vec3 eye = vec3(0.0, 0.0, 5.0);
+	vec3 up = vec3(0.0, 1.0, 0.0);
+	Transform::up(angle, eye, up);
+	testAssert(vec3(0.0, 5.0, 0.0), eye, "eye coordinates incorrect");
+	testAssert(vec3(0.0, 0.0, -1.0), up, "up coordinates incorrect");
+}
+
+void upByNegativeAngle() {
+	std::cout << "upByNegativeAngle:" << std::endl;
+	const float angle = -90.0;
+	vec3 eye = vec3(0.0, 0.0, 5.0);
+	vec3 up = vec3(0.0, 1.0, 0.0);
+	Transform::up(angle, eye, up);
+	testAssert(vec3(0.0, -5.0, 0.0), eye, "eye coordinates incorrect");
+	testAssert(vec3(0.0, 0.0, 1.0), up, "up coordinates incorrect");
+}
+
+void lookAt() {
+	std::cout << "lookAt:" << std::endl;
+	const vec3 eye = vec3(1.0, 0.0, 0.0);
+	const vec3 up = vec3(0.0, 1.0, 0.0);
+	const vec3 center = vec3(0.0, 0.0, 0.0);
+	const mat4 result = Transform::lookAt(eye, up);
+	testAssert(glm::lookAt(eye,center,up), result, "lookAt matrix incorrect");
+}
+
+int main(int argc,char* argv[]) {
+	leftByPositiveAngle();
+	leftByNegativeAngle();
+	upByPositiveAngle();
+	upByNegativeAngle();
+	lookAt();
+}
