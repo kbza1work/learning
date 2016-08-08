af8ef42577b3d8697b54bf3c3a0a71c2770a8384
Brent Houghton <brent@slixbits.com>
2016-08-07 21:20:32 -0700

    Move tests into separate files from program code
    The tests were preventing the program from compiling.

--- edX/CSE167x Computer Graphics/Homework 2/Transform.cpp
+++ edX/CSE167x Computer Graphics/Homework 2/Transform.cpp
@@ -34,177 +34,11 @@ void Transform::left(float degrees, vec3& eye, vec3& up)
   eye = Transform::rotate(degrees, rotationAxis) * eye;
 }
 
-TEST_CASE("Transform::left()", "[transform][left]") {
-    SECTION("by a positive angle around the y-axis") {
-        const float angle = 90.0;
-        vec3 eye = vec3(0.0, 0.0, 5.0);
-        vec3 up = vec3(0.0, 5.0, 0.0);
-        Transform::left(angle, eye, up);
-
-        SECTION("eye x-component") {
-            REQUIRE(eye.x == Approx(5.0));
-        }
-        SECTION("eye y-component") {
-            REQUIRE(eye.y == Approx(0.0));
-        }
-        SECTION("eye z-component") {
-            REQUIRE(eye.z == Approx(0.0));
-        }
-
-        SECTION("up x-component") {
-            REQUIRE(up.x == Approx(0.0));
-        }
-        SECTION("up y-component") {
-            REQUIRE(up.y == Approx(5.0));
-        }
-        SECTION("up z-component") {
-            REQUIRE(up.z == Approx(0.0));
-        }
-    }
-
-    SECTION("by a negative angle around the y-axis") {
-        const float angle = -90.0;
-        vec3 eye = vec3(0.0, 0.0, 5.0);
-        vec3 up = vec3(0.0, 5.0, 0.0);
-        Transform::left(angle, eye, up);
-
-        SECTION("eye x-component") {
-            REQUIRE(eye.x == Approx(-5.0));
-        }
-        SECTION("eye y-component") {
-            REQUIRE(eye.y == Approx(0.0));
-        }
-        SECTION("eye z-component") {
-            REQUIRE(eye.z == Approx(0.0));
-        }
-
-        SECTION("up x-component") {
-            REQUIRE(up.x == Approx(0.0));
-        }
-        SECTION("up y-component") {
-            REQUIRE(up.y == Approx(5.0));
-        }
-        SECTION("up z-component") {
-            REQUIRE(up.z == Approx(0.0));
-        }
-    }
-
-    SECTION("by a canted positive angle") {
-        const float angle = 90.0;
-        vec3 eye = vec3(0.0, 0.0, 5.0);
-        vec3 up = vec3(0.5, 0.5, 0.0);
-        Transform::left(angle, eye, up);
-
-        SECTION("eye x-component") {
-            REQUIRE(eye.x == Approx(3.535533906));
-        }
-        SECTION("eye y-component") {
-            REQUIRE(eye.y == Approx(-3.535533906));
-        }
-        SECTION("eye z-component") {
-            REQUIRE(eye.z == Approx(0.0));
-        }
-
-        SECTION("up x-component") {
-            REQUIRE(up.x == Approx(0.5));
-        }
-        SECTION("up y-component") {
-            REQUIRE(up.y == Approx(0.5));
-        }
-        SECTION("up z-component") {
-            REQUIRE(up.z == Approx(0.0));
-        }
-    }
-}
-
 void Transform::up(float degrees, vec3& eye, vec3& up)
 {
   const vec3 rotationAxis = glm::normalize(glm::cross(eye, up));
   eye = Transform::rotate(degrees, rotationAxis) * eye;
   up = Transform::rotate(degrees, rotationAxis) * up;
-}
-
-TEST_CASE("Transform::up()", "[transform][up]") {
-    SECTION("by a positive angle around the y-axis") {
-        const float angle = 90.0;
-        vec3 eye = vec3(0.0, 0.0, 5.0);
-        vec3 up = vec3(0.0, 5.0, 0.0);
-        Transform::up(angle, eye, up);
-
-        SECTION("eye x-component") {
-            REQUIRE(eye.x == Approx(0.0));
-        }
-        SECTION("eye y-component") {
-            REQUIRE(eye.y == Approx(5.0));
-        }
-        SECTION("eye z-component") {
-            REQUIRE(eye.z == Approx(0.0));
-        }
-
-        SECTION("up x-component") {
-            REQUIRE(up.x == Approx(0.0));
-        }
-        SECTION("up y-component") {
-            REQUIRE(up.y == Approx(0.0));
-        }
-        SECTION("up z-component") {
-            REQUIRE(up.z == Approx(-5.0));
-        }
-    }
-
-    SECTION("by a negative angle around the y-axis") {
-        const float angle = -90.0;
-        vec3 eye = vec3(0.0, 0.0, 5.0);
-        vec3 up = vec3(0.0, 5.0, 0.0);
-        Transform::up(angle, eye, up);
-
-        SECTION("eye x-component") {
-            REQUIRE(eye.x == Approx(0.0));
-        }
-        SECTION("eye y-component") {
-            REQUIRE(eye.y == Approx(-5.0));
-        }
-        SECTION("eye z-component") {
-            REQUIRE(eye.z == Approx(0.0));
-        }
-
-        SECTION("up x-component") {
-            REQUIRE(up.x == Approx(0.0));
-        }
-        SECTION("up y-component") {
-            REQUIRE(up.y == Approx(0.0));
-        }
-        SECTION("up z-component") {
-            REQUIRE(up.z == Approx(5.0));
-        }
-    }
-
-    SECTION("by a canted positive angle") {
-        const float angle = 90.0;
-        vec3 eye = vec3(5.0, 5.0, 0.0);
-        vec3 up = vec3(-0.5, 0.5, 0.0);
-        Transform::up(angle, eye, up);
-
-        SECTION("eye x-component") {
-            REQUIRE(eye.x == Approx(-5.0));
-        }
-        SECTION("eye y-component") {
-            REQUIRE(eye.y == Approx(5.0));
-        }
-        SECTION("eye z-component") {
-            REQUIRE(eye.z == Approx(0.0));
-        }
-
-        SECTION("up x-component") {
-            REQUIRE(up.x == Approx(-0.5));
-        }
-        SECTION("up y-component") {
-            REQUIRE(up.y == Approx(-0.5));
-        }
-        SECTION("up z-component") {
-            REQUIRE(up.z == Approx(0.0));
-        }
-    }
 }
 
 mat4 Transform::lookAt(const vec3 &eye, const vec3 &center, const vec3 &up)
@@ -227,17 +61,6 @@ mat4 Transform::lookAt(const vec3 &eye, const vec3 &center, const vec3 &up)
   return mat4(column0, column1, column2, column3);
 }
 
-TEST_CASE("Transform::lookAt()", "[transform][lookAt]") {
-    SECTION("looking at the origin") {
-        const vec3 eye = vec3(1.0, 0.0, 0.0);
-        const vec3 up = vec3(0.0, 1.0, 0.0);
-        const vec3 center = vec3(0.0, 0.0, 0.0);
-        const mat4 result = Transform::lookAt(eye, center, up);
-        const mat4 expected = glm::lookAt(eye, center, up);
-        REQUIRE(*(glm::value_ptr(result)) == *(glm::value_ptr(expected)));
-    }
-}
-
 mat4 Transform::perspective(float fovy, float aspect, float zNear, float zFar)
 {
   mat4 ret;
@@ -256,84 +79,6 @@ mat4 Transform::scale(const float &sx, const float &sy, const float &sz)
     );
 }
 
-TEST_CASE("Transform::scale()", "[transform][scale]") {
-    SECTION("by a positive factor only in the x-direction") {
-        const vec4 original = vec4(2.8, -1.5, 5.3, 1.0);
-        const vec4 result =
-            original * Transform::scale(2.0, 1.0, 1.0);
-
-        SECTION("x-component") {
-            REQUIRE(result.x == Approx(5.6));
-        }
-        SECTION("y-component") {
-            REQUIRE(result.y == Approx(-1.5));
-        }
-        SECTION("z-component") {
-            REQUIRE(result.z == Approx(5.3));
-        }
-        SECTION("w-component") {
-            REQUIRE(result.w == Approx(1.0));
-        }
-    }
-
-    SECTION("by a uniform positive factor") {
-        const vec4 original = vec4(2.8, -1.5, 5.3, 1.0);
-        const vec4 result =
-            original * Transform::scale(3.0, 3.0, 3.0);
-
-        SECTION("x-component") {
-            REQUIRE(result.x == Approx(8.4));
-        }
-        SECTION("y-component") {
-            REQUIRE(result.y == Approx(-4.5));
-        }
-        SECTION("z-component") {
-            REQUIRE(result.z == Approx(15.9));
-        }
-        SECTION("w-component") {
-            REQUIRE(result.w == Approx(1.0));
-        }
-    }
-
-    SECTION("mirroring the y-component") {
-        const vec4 original = vec4(2.8, -1.5, 5.3, 1.0);
-        const vec4 result =
-            original * Transform::scale(1.0, -3.0, 1.0);
-
-        SECTION("x-component") {
-            REQUIRE(result.x == Approx(2.8));
-        }
-        SECTION("y-component") {
-            REQUIRE(result.y == Approx(4.5));
-        }
-        SECTION("z-component") {
-            REQUIRE(result.z == Approx(5.3));
-        }
-        SECTION("w-component") {
-            REQUIRE(result.w == Approx(1.0));
-        }
-    }
-
-    SECTION("decimating the z-component") {
-        const vec4 original = vec4(2.8, -1.5, 5.3, 1.0);
-        const vec4 result =
-            original * Transform::scale(1.0, 1.0, 0.0);
-
-        SECTION("x-component") {
-            REQUIRE(result.x == Approx(2.8));
-        }
-        SECTION("y-component") {
-            REQUIRE(result.y == Approx(-1.5));
-        }
-        SECTION("z-component") {
-            REQUIRE(result.z == Approx(0.0));
-        }
-        SECTION("w-component") {
-            REQUIRE(result.w == Approx(1.0));
-        }
-    }
-}
-
 mat4 Transform::translate(const float &tx, const float &ty, const float &tz)
 {
     return mat4(
@@ -342,46 +87,6 @@ mat4 Transform::translate(const float &tx, const float &ty, const float &tz)
         0.0, 0.0, 1.0, tz,
         0.0, 0.0, 0.0, 1.0
     );
-}
-
-TEST_CASE("Transform::translate()", "[transform][translate]") {
-    SECTION("in the x direction") {
-        const vec4 position = vec4(2.1, 3.5, -4.9, 1.0);
-        vec4 result = position * Transform::translate(5.2, 0.0, 0.0);
-        result = result/(result.w);
-
-        SECTION("x-component") {
-            REQUIRE(result.x == Approx(7.3));
-        }
-        SECTION("y-component") {
-            REQUIRE(result.y == Approx(3.5));
-        }
-        SECTION("z-component") {
-            REQUIRE(result.z == Approx(-4.9));
-        }
-        SECTION("w-component") {
-            REQUIRE(result.w == Approx(1.0));
-        }
-    }
-
-    SECTION("diagonally") {
-        const vec4 position = vec4(2.1, 3.5, -4.9, 1.0);
-        vec4 result = position * Transform::translate(0.8, -1.9, -9.8);
-        result = result/(result.w);
-
-        SECTION("x-component") {
-            REQUIRE(result.x == Approx(2.9));
-        }
-        SECTION("y-component") {
-            REQUIRE(result.y == Approx(1.6));
-        }
-        SECTION("z-component") {
-            REQUIRE(result.z == Approx(-14.7));
-        }
-        SECTION("w-component") {
-            REQUIRE(result.w == Approx(1.0));
-        }
-    }
 }
 
 // To normalize the up direction and construct a coordinate frame.
@@ -408,3 +113,4 @@ Transform::~Transform()
 {
 
 }
+
---
+++ edX/CSE167x Computer Graphics/Homework 2/TransformTests.cpp
@@ -0,0 +1,298 @@
+#include "Transform.h"
+#include <glm/gtc/type_ptr.hpp>
+
+TEST_CASE("Transform::left()", "[transform][left]") {
+    SECTION("by a positive angle around the y-axis") {
+        const float angle = 90.0;
+        vec3 eye = vec3(0.0, 0.0, 5.0);
+        vec3 up = vec3(0.0, 5.0, 0.0);
+        Transform::left(angle, eye, up);
+
+        SECTION("eye x-component") {
+            REQUIRE(eye.x == Approx(5.0));
+        }
+        SECTION("eye y-component") {
+            REQUIRE(eye.y == Approx(0.0));
+        }
+        SECTION("eye z-component") {
+            REQUIRE(eye.z == Approx(0.0));
+        }
+
+        SECTION("up x-component") {
+            REQUIRE(up.x == Approx(0.0));
+        }
+        SECTION("up y-component") {
+            REQUIRE(up.y == Approx(5.0));
+        }
+        SECTION("up z-component") {
+            REQUIRE(up.z == Approx(0.0));
+        }
+    }
+
+    SECTION("by a negative angle around the y-axis") {
+        const float angle = -90.0;
+        vec3 eye = vec3(0.0, 0.0, 5.0);
+        vec3 up = vec3(0.0, 5.0, 0.0);
+        Transform::left(angle, eye, up);
+
+        SECTION("eye x-component") {
+            REQUIRE(eye.x == Approx(-5.0));
+        }
+        SECTION("eye y-component") {
+            REQUIRE(eye.y == Approx(0.0));
+        }
+        SECTION("eye z-component") {
+            REQUIRE(eye.z == Approx(0.0));
+        }
+
+        SECTION("up x-component") {
+            REQUIRE(up.x == Approx(0.0));
+        }
+        SECTION("up y-component") {
+            REQUIRE(up.y == Approx(5.0));
+        }
+        SECTION("up z-component") {
+            REQUIRE(up.z == Approx(0.0));
+        }
+    }
+
+    SECTION("by a canted positive angle") {
+        const float angle = 90.0;
+        vec3 eye = vec3(0.0, 0.0, 5.0);
+        vec3 up = vec3(0.5, 0.5, 0.0);
+        Transform::left(angle, eye, up);
+
+        SECTION("eye x-component") {
+            REQUIRE(eye.x == Approx(3.535533906));
+        }
+        SECTION("eye y-component") {
+            REQUIRE(eye.y == Approx(-3.535533906));
+        }
+        SECTION("eye z-component") {
+            REQUIRE(eye.z == Approx(0.0));
+        }
+
+        SECTION("up x-component") {
+            REQUIRE(up.x == Approx(0.5));
+        }
+        SECTION("up y-component") {
+            REQUIRE(up.y == Approx(0.5));
+        }
+        SECTION("up z-component") {
+            REQUIRE(up.z == Approx(0.0));
+        }
+    }
+}
+
+TEST_CASE("Transform::up()", "[transform][up]") {
+    SECTION("by a positive angle around the y-axis") {
+        const float angle = 90.0;
+        vec3 eye = vec3(0.0, 0.0, 5.0);
+        vec3 up = vec3(0.0, 5.0, 0.0);
+        Transform::up(angle, eye, up);
+
+        SECTION("eye x-component") {
+            REQUIRE(eye.x == Approx(0.0));
+        }
+        SECTION("eye y-component") {
+            REQUIRE(eye.y == Approx(5.0));
+        }
+        SECTION("eye z-component") {
+            REQUIRE(eye.z == Approx(0.0));
+        }
+
+        SECTION("up x-component") {
+            REQUIRE(up.x == Approx(0.0));
+        }
+        SECTION("up y-component") {
+            REQUIRE(up.y == Approx(0.0));
+        }
+        SECTION("up z-component") {
+            REQUIRE(up.z == Approx(-5.0));
+        }
+    }
+
+    SECTION("by a negative angle around the y-axis") {
+        const float angle = -90.0;
+        vec3 eye = vec3(0.0, 0.0, 5.0);
+        vec3 up = vec3(0.0, 5.0, 0.0);
+        Transform::up(angle, eye, up);
+
+        SECTION("eye x-component") {
+            REQUIRE(eye.x == Approx(0.0));
+        }
+        SECTION("eye y-component") {
+            REQUIRE(eye.y == Approx(-5.0));
+        }
+        SECTION("eye z-component") {
+            REQUIRE(eye.z == Approx(0.0));
+        }
+
+        SECTION("up x-component") {
+            REQUIRE(up.x == Approx(0.0));
+        }
+        SECTION("up y-component") {
+            REQUIRE(up.y == Approx(0.0));
+        }
+        SECTION("up z-component") {
+            REQUIRE(up.z == Approx(5.0));
+        }
+    }
+
+    SECTION("by a canted positive angle") {
+        const float angle = 90.0;
+        vec3 eye = vec3(5.0, 5.0, 0.0);
+        vec3 up = vec3(-0.5, 0.5, 0.0);
+        Transform::up(angle, eye, up);
+
+        SECTION("eye x-component") {
+            REQUIRE(eye.x == Approx(-5.0));
+        }
+        SECTION("eye y-component") {
+            REQUIRE(eye.y == Approx(5.0));
+        }
+        SECTION("eye z-component") {
+            REQUIRE(eye.z == Approx(0.0));
+        }
+
+        SECTION("up x-component") {
+            REQUIRE(up.x == Approx(-0.5));
+        }
+        SECTION("up y-component") {
+            REQUIRE(up.y == Approx(-0.5));
+        }
+        SECTION("up z-component") {
+            REQUIRE(up.z == Approx(0.0));
+        }
+    }
+}
+
+TEST_CASE("Transform::lookAt()", "[transform][lookAt]") {
+    SECTION("looking at the origin") {
+        const vec3 eye = vec3(1.0, 0.0, 0.0);
+        const vec3 up = vec3(0.0, 1.0, 0.0);
+        const vec3 center = vec3(0.0, 0.0, 0.0);
+        const mat4 result = Transform::lookAt(eye, center, up);
+        const mat4 expected = glm::lookAt(eye, center, up);
+        REQUIRE(*(glm::value_ptr(result)) == *(glm::value_ptr(expected)));
+    }
+}
+
+TEST_CASE("Transform::scale()", "[transform][scale]") {
+    SECTION("by a positive factor only in the x-direction") {
+        const vec4 original = vec4(2.8, -1.5, 5.3, 1.0);
+        const vec4 result =
+            original * Transform::scale(2.0, 1.0, 1.0);
+
+        SECTION("x-component") {
+            REQUIRE(result.x == Approx(5.6));
+        }
+        SECTION("y-component") {
+            REQUIRE(result.y == Approx(-1.5));
+        }
+        SECTION("z-component") {
+            REQUIRE(result.z == Approx(5.3));
+        }
+        SECTION("w-component") {
+            REQUIRE(result.w == Approx(1.0));
+        }
+    }
+
+    SECTION("by a uniform positive factor") {
+        const vec4 original = vec4(2.8, -1.5, 5.3, 1.0);
+        const vec4 result =
+            original * Transform::scale(3.0, 3.0, 3.0);
+
+        SECTION("x-component") {
+            REQUIRE(result.x == Approx(8.4));
+        }
+        SECTION("y-component") {
+            REQUIRE(result.y == Approx(-4.5));
+        }
+        SECTION("z-component") {
+            REQUIRE(result.z == Approx(15.9));
+        }
+        SECTION("w-component") {
+            REQUIRE(result.w == Approx(1.0));
+        }
+    }
+
+    SECTION("mirroring the y-component") {
+        const vec4 original = vec4(2.8, -1.5, 5.3, 1.0);
+        const vec4 result =
+            original * Transform::scale(1.0, -3.0, 1.0);
+
+        SECTION("x-component") {
+            REQUIRE(result.x == Approx(2.8));
+        }
+        SECTION("y-component") {
+            REQUIRE(result.y == Approx(4.5));
+        }
+        SECTION("z-component") {
+            REQUIRE(result.z == Approx(5.3));
+        }
+        SECTION("w-component") {
+            REQUIRE(result.w == Approx(1.0));
+        }
+    }
+
+    SECTION("decimating the z-component") {
+        const vec4 original = vec4(2.8, -1.5, 5.3, 1.0);
+        const vec4 result =
+            original * Transform::scale(1.0, 1.0, 0.0);
+
+        SECTION("x-component") {
+            REQUIRE(result.x == Approx(2.8));
+        }
+        SECTION("y-component") {
+            REQUIRE(result.y == Approx(-1.5));
+        }
+        SECTION("z-component") {
+            REQUIRE(result.z == Approx(0.0));
+        }
+        SECTION("w-component") {
+            REQUIRE(result.w == Approx(1.0));
+        }
+    }
+}
+
+TEST_CASE("Transform::translate()", "[transform][translate]") {
+    SECTION("in the x direction") {
+        const vec4 position = vec4(2.1, 3.5, -4.9, 1.0);
+        vec4 result = position * Transform::translate(5.2, 0.0, 0.0);
+        result = result/(result.w);
+
+        SECTION("x-component") {
+            REQUIRE(result.x == Approx(7.3));
+        }
+        SECTION("y-component") {
+            REQUIRE(result.y == Approx(3.5));
+        }
+        SECTION("z-component") {
+            REQUIRE(result.z == Approx(-4.9));
+        }
+        SECTION("w-component") {
+            REQUIRE(result.w == Approx(1.0));
+        }
+    }
+
+    SECTION("diagonally") {
+        const vec4 position = vec4(2.1, 3.5, -4.9, 1.0);
+        vec4 result = position * Transform::translate(0.8, -1.9, -9.8);
+        result = result/(result.w);
+
+        SECTION("x-component") {
+            REQUIRE(result.x == Approx(2.9));
+        }
+        SECTION("y-component") {
+            REQUIRE(result.y == Approx(1.6));
+        }
+        SECTION("z-component") {
+            REQUIRE(result.z == Approx(-14.7));
+        }
+        SECTION("w-component") {
+            REQUIRE(result.w == Approx(1.0));
+        }
+    }
+}
+
