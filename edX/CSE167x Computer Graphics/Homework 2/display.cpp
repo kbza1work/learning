137bf68106f6b9526c72ff07369e888013b5c115
Brent Houghton <brent@slixbits.com>
2016-08-25 14:32:05 -0700

    Remove trailing spaces from provided code for Homework 2

--- edX/CSE167x Computer Graphics/Homework 2/display.cpp
+++ edX/CSE167x Computer Graphics/Homework 2/display.cpp
@@ -5,7 +5,7 @@
 
 // This file is display.cpp.  It includes the skeleton for the display routine
 
-// Basic includes to get this file to work.  
+// Basic includes to get this file to work.
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -15,51 +15,51 @@
 #include <GL/glut.h>
 #include "Transform.h"
 
-using namespace std ; 
+using namespace std ;
 #include "variables.h"
 #include "readfile.h"
 
-// New helper transformation function to transform vector by modelview 
+// New helper transformation function to transform vector by modelview
 // May be better done using newer glm functionality.
-// Provided for your convenience.  Use is optional.  
-// Some of you may want to use the more modern routines in readfile.cpp 
-// that can also be used.  
-void transformvec (const GLfloat input[4], GLfloat output[4]) 
+// Provided for your convenience.  Use is optional.
+// Some of you may want to use the more modern routines in readfile.cpp
+// that can also be used.
+void transformvec (const GLfloat input[4], GLfloat output[4])
 {
   GLfloat modelview[16]; // in column major order
-  glGetFloatv(GL_MODELVIEW_MATRIX, modelview); 
+  glGetFloatv(GL_MODELVIEW_MATRIX, modelview);
 
   for (int i = 0 ; i < 4 ; i++) {
-    output[i] = 0; 
+    output[i] = 0;
     for (int j = 0 ; j < 4 ; j++) {
-      output[i] += modelview[4*j+i] * input[j]; 
+      output[i] += modelview[4*j+i] * input[j];
     }
   }
 }
 
-void display() 
+void display()
 {
   glClearColor(0, 0, 1, 0);
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-  // I'm including the basic matrix setup for model view to 
-  // give some sense of how this works.  
+  // I'm including the basic matrix setup for model view to
+  // give some sense of how this works.
 
   glMatrixMode(GL_MODELVIEW);
-  mat4 mv; 
+  mat4 mv;
 
   // Either use the built-in lookAt function or the lookAt implemented by the user.
   if (useGlu) {
-    mv = glm::lookAt(eye,center,up); 
+    mv = glm::lookAt(eye,center,up);
   } else {
-    mv = Transform::lookAt(eye,center,up); 
+    mv = Transform::lookAt(eye,center,up);
   }
 
-  glLoadMatrixf(&mv[0][0]); 
+  glLoadMatrixf(&mv[0][0]);
 
-  // Lights are transformed by current modelview matrix. 
-  // The shader can't do this globally. 
-  // So we need to do so manually.  
+  // Lights are transformed by current modelview matrix.
+  // The shader can't do this globally.
+  // So we need to do so manually.
   if (numused) {
     glUniform1i(enablelighting,true);
 
@@ -70,13 +70,13 @@ void display()
     // Remember that light positions must be transformed by modelview.  
 
   } else {
-    glUniform1i(enablelighting,false); 
+    glUniform1i(enablelighting,false);
   }
 
-  // Transformations for objects, involving translation and scaling 
-  mat4 sc(1.0) , tr(1.0), transf(1.0); 
-  sc = Transform::scale(sx,sy,1.0); 
-  tr = Transform::translate(tx,ty,0.0); 
+  // Transformations for objects, involving translation and scaling
+  mat4 sc(1.0) , tr(1.0), transf(1.0);
+  sc = Transform::scale(sx,sy,1.0);
+  tr = Transform::translate(tx,ty,0.0);
 
   // YOUR CODE FOR HW 2 HERE.  
   // You need to use scale, translate and modelview to 
@@ -88,23 +88,23 @@ void display()
   for (int i = 0 ; i < numobjects ; i++) {
     object* obj = &(objects[i]); // Grabs an object struct.
 
-    // YOUR CODE FOR HW 2 HERE. 
-    // Set up the object transformations 
+    // YOUR CODE FOR HW 2 HERE.
+    // Set up the object transformations
     // And pass in the appropriate material properties
     // Again glUniform() related functions will be useful
 
     // Actually draw the object
-    // We provide the actual glut drawing functions for you.  
+    // We provide the actual glut drawing functions for you.
     // Remember that obj->type is notation for accessing struct fields
     if (obj->type == cube) {
-      glutSolidCube(obj->size); 
+      glutSolidCube(obj->size);
     }
     else if (obj->type == sphere) {
-      const int tessel = 20; 
-      glutSolidSphere(obj->size, tessel, tessel); 
+      const int tessel = 20;
+      glutSolidSphere(obj->size, tessel, tessel);
     }
     else if (obj->type == teapot) {
-      glutSolidTeapot(obj->size); 
+      glutSolidTeapot(obj->size);
     }
 
   }
--- edX/CSE167x Computer Graphics/Homework 2/main.cpp
+++ edX/CSE167x Computer Graphics/Homework 2/main.cpp
@@ -17,19 +17,19 @@
 #include <FreeImage.h>
 #include "UCSD/grader.h"
 
-using namespace std; 
+using namespace std;
 
-// Main variables in the program.  
-#define MAINPROGRAM 
-#include "variables.h" 
-#include "readfile.h" // prototypes for readfile.cpp  
-void display(void);  // prototype for display function.  
+// Main variables in the program.
+#define MAINPROGRAM
+#include "variables.h"
+#include "readfile.h" // prototypes for readfile.cpp
+void display(void);  // prototype for display function.
 
 Grader grader;
 bool allowGrader = false;
 
-// Uses the Projection matrices (technically deprecated) to set perspective 
-// We could also do this in a more modern fashion with glm.  
+// Uses the Projection matrices (technically deprecated) to set perspective
+// We could also do this in a more modern fashion with glm.
 void reshape(int width, int height)
 {
   w = width;
@@ -50,10 +50,10 @@ void reshape(int width, int height)
   glViewport(0, 0, w, h);
 }
 
-void saveScreenshot(string fname) 
+void saveScreenshot(string fname)
 {
   int pix = w * h;
-  BYTE pixels[3*pix];	
+  BYTE pixels[3*pix];
   glReadBuffer(GL_FRONT);
   glReadPixels(0,0,w,h,GL_BGR,GL_UNSIGNED_BYTE, pixels);
 
@@ -65,19 +65,19 @@ void saveScreenshot(string fname)
 }
 
 
-void printHelp() 
+void printHelp()
 {
-  std::cout << "\npress 'h' to print this message again.\n" 
-    << "press '+' or '-' to change the amount of rotation that\noccurs with each arrow press.\n" 
+  std::cout << "\npress 'h' to print this message again.\n"
+    << "press '+' or '-' to change the amount of rotation that\noccurs with each arrow press.\n"
     << "press 'i' to run image grader test cases\n"
-    << "press 'g' to switch between using glm::lookAt and glm::Perspective or your own LookAt.\n"       
+    << "press 'g' to switch between using glm::lookAt and glm::Perspective or your own LookAt.\n"
     << "press 'r' to reset the transformations.\n"
     << "press 'v' 't' 's' to do view [default], translate, scale.\n"
-    << "press ESC to quit.\n";      
+    << "press ESC to quit.\n";
 }
 
 
-void keyboard(unsigned char key, int x, int y) 
+void keyboard(unsigned char key, int x, int y)
 {
   switch(key) {
     case '+':
@@ -86,13 +86,13 @@ void keyboard(unsigned char key, int x, int y)
       break;
     case '-':
       amount--;
-      std::cout << "amount set to " << amount << "\n"; 
+      std::cout << "amount set to " << amount << "\n";
       break;
     case 'i':
       if(useGlu) {
         std::cout << "Please disable glm::LookAt by pressing 'g'"
           << " before running tests\n";
-      }	
+      }
       else if(!allowGrader) {
         std::cout << "Error: no input file specified for grader\n";
       } else {
@@ -103,9 +103,9 @@ void keyboard(unsigned char key, int x, int y)
       break;
     case 'g':
       useGlu = !useGlu;
-      reshape(w,h); 
-      std::cout << "Using glm::LookAt and glm::Perspective set to: " 
-        << (useGlu ? " true " : " false ") << "\n"; 
+      reshape(w,h);
+      std::cout << "Using glm::LookAt and glm::Perspective set to: "
+        << (useGlu ? " true " : " false ") << "\n";
       break;
     case 'h':
       printHelp();
@@ -113,54 +113,54 @@ void keyboard(unsigned char key, int x, int y)
     case 27:  // Escape to quit
       exit(0);
       break;
-    case 'r': // reset eye and up vectors, scale and translate. 
-      eye = eyeinit; 
-      up = upinit; 
+    case 'r': // reset eye and up vectors, scale and translate.
+      eye = eyeinit;
+      up = upinit;
       amount = amountinit;
       transop = view;
-      sx = sy = 1.0; 
-      tx = ty = 0.0; 
-      break;   
-    case 'v': 
+      sx = sy = 1.0;
+      tx = ty = 0.0;
+      break;
+    case 'v':
       transop = view;
-      std::cout << "Operation is set to View\n"; 
-      break; 
+      std::cout << "Operation is set to View\n";
+      break;
     case 't':
-      transop = translate; 
-      std::cout << "Operation is set to Translate\n"; 
-      break; 
+      transop = translate;
+      std::cout << "Operation is set to Translate\n";
+      break;
     case 's':
-      transop = scale; 
-      std::cout << "Operation is set to Scale\n"; 
-      break; 
+      transop = scale;
+      std::cout << "Operation is set to Scale\n";
+      break;
   }
   glutPostRedisplay();
 }
 
-//  You will need to enter code for the arrow keys 
+//  You will need to enter code for the arrow keys
 //  When an arrow key is pressed, it will call your transform functions
-void specialKey(int key, int x, int y) 
+void specialKey(int key, int x, int y)
 {
   switch(key) {
     case 100: //left
       if (transop == view) Transform::left(amount, eye,  up);
-      else if (transop == scale) sx -= amount * 0.01; 
-      else if (transop == translate) tx -= amount * 0.01; 
+      else if (transop == scale) sx -= amount * 0.01;
+      else if (transop == translate) tx -= amount * 0.01;
       break;
     case 101: //up
       if (transop == view) Transform::up(amount,  eye,  up);
-      else if (transop == scale) sy += amount * 0.01; 
-      else if (transop == translate) ty += amount * 0.01; 
+      else if (transop == scale) sy += amount * 0.01;
+      else if (transop == translate) ty += amount * 0.01;
       break;
     case 102: //right
       if (transop == view) Transform::left(-amount, eye,  up);
-      else if (transop == scale) sx += amount * 0.01; 
-      else if (transop == translate) tx += amount * 0.01; 
+      else if (transop == scale) sx += amount * 0.01;
+      else if (transop == translate) tx += amount * 0.01;
       break;
     case 103: //down
       if (transop == view) Transform::up(-amount,  eye,  up);
-      else if (transop == scale) sy -= amount * 0.01; 
-      else if (transop == translate) ty -= amount * 0.01; 
+      else if (transop == scale) sy -= amount * 0.01;
+      else if (transop == translate) ty -= amount * 0.01;
       break;
   }
   glutPostRedisplay();
@@ -171,23 +171,23 @@ void init()
   // Initialize shaders
   vertexshader = initshaders(GL_VERTEX_SHADER, "shaders/light.vert.glsl");
   fragmentshader = initshaders(GL_FRAGMENT_SHADER, "shaders/light.frag.glsl");
-  shaderprogram = initprogram(vertexshader, fragmentshader); 
+  shaderprogram = initprogram(vertexshader, fragmentshader);
   enablelighting = glGetUniformLocation(shaderprogram,"enablelighting");
-  lightpos = glGetUniformLocation(shaderprogram,"lightposn");       
-  lightcol = glGetUniformLocation(shaderprogram,"lightcolor");       
-  numusedcol = glGetUniformLocation(shaderprogram,"numused");       
-  ambientcol = glGetUniformLocation(shaderprogram,"ambient");       
-  diffusecol = glGetUniformLocation(shaderprogram,"diffuse");       
-  specularcol = glGetUniformLocation(shaderprogram,"specular");       
-  emissioncol = glGetUniformLocation(shaderprogram,"emission");       
-  shininesscol = glGetUniformLocation(shaderprogram,"shininess");       
+  lightpos = glGetUniformLocation(shaderprogram,"lightposn");
+  lightcol = glGetUniformLocation(shaderprogram,"lightcolor");
+  numusedcol = glGetUniformLocation(shaderprogram,"numused");
+  ambientcol = glGetUniformLocation(shaderprogram,"ambient");
+  diffusecol = glGetUniformLocation(shaderprogram,"diffuse");
+  specularcol = glGetUniformLocation(shaderprogram,"specular");
+  emissioncol = glGetUniformLocation(shaderprogram,"emission");
+  shininesscol = glGetUniformLocation(shaderprogram,"shininess");
 }
 
-int main(int argc, char* argv[]) 
+int main(int argc, char* argv[])
 {
   if (argc < 2) {
-    cerr << "Usage: transforms scenefile [grader input (optional)]\n"; 
-    exit(-1); 
+    cerr << "Usage: transforms scenefile [grader input (optional)]\n";
+    exit(-1);
   }
 
   FreeImage_Initialise();
@@ -195,7 +195,7 @@ int main(int argc, char* argv[])
   glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
   glutCreateWindow("HW2: Scene Viewer");
   init();
-  readfile(argv[1]); 
+  readfile(argv[1]);
   glutDisplayFunc(display);
   glutSpecialFunc(specialKey);
   glutKeyboardFunc(keyboard);
