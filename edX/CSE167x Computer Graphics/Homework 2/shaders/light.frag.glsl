8b85345fa2965f0a9f1438bf3c55d9f972703cdf
Brent Houghton <brent@slixbits.com>
2016-08-26 16:49:08 -0700

    Remove trailing whitespace from the fragment shader

--- edX/CSE167x Computer Graphics/Homework 2/shaders/light.frag.glsl
+++ edX/CSE167x Computer Graphics/Homework 2/shaders/light.frag.glsl
@@ -1,43 +1,43 @@
-# version 130 
+# version 130
 
-/* This is the fragment shader for reading in a scene description, including 
-   lighting.  Uniform lights are specified from the main program, and used in 
+/* This is the fragment shader for reading in a scene description, including
+   lighting.  Uniform lights are specified from the main program, and used in
    the shader.  As well as the material parameters of the object.  */
 
 in vec4 color;
-in vec3 mynormal; 
-in vec4 myvertex; 
+in vec3 mynormal;
+in vec4 myvertex;
 
-const int numLights = 10; 
+const int numLights = 10;
 uniform bool enablelighting; // are we lighting at all (global).
-uniform vec4 lightposn[numLights]; // positions of lights 
+uniform vec4 lightposn[numLights]; // positions of lights
 uniform vec4 lightcolor[numLights]; // colors of lights
 uniform int numused;               // number of lights used
 
-// Now, set the material parameters.  These could be varying and/or bound to 
-// a buffer.  But for now, I'll just make them uniform.  
-// I use ambient, diffuse, specular, shininess as in OpenGL.  
-// But, the ambient is just additive and doesn't multiply the lights.  
+// Now, set the material parameters.  These could be varying and/or bound to
+// a buffer.  But for now, I'll just make them uniform.
+// I use ambient, diffuse, specular, shininess as in OpenGL.
+// But, the ambient is just additive and doesn't multiply the lights.
 
-uniform vec4 ambient; 
-uniform vec4 diffuse; 
-uniform vec4 specular; 
-uniform vec4 emission; 
-uniform float shininess; 
+uniform vec4 ambient;
+uniform vec4 diffuse;
+uniform vec4 specular;
+uniform vec4 emission;
+uniform float shininess;
 
-void main (void) 
-{       
-    if (enablelighting) {       
-        vec4 finalcolor; 
+void main (void)
+{
+    if (enablelighting) {
+        vec4 finalcolor;
 
         // YOUR CODE FOR HW 2 HERE
         // A key part is implementation of the fragment shader
 
         // Color all pixels black for now, remove this in your implementation!
-        finalcolor = vec4(0,0,0,1); 
+        finalcolor = vec4(0,0,0,1);
 
-        gl_FragColor = finalcolor; 
+        gl_FragColor = finalcolor;
     } else {
-        gl_FragColor = color; 
+        gl_FragColor = color;
     }
 }
