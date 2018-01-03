fd8139c5b79909b062d4e23443bbf2d16557f4c4
Brent Houghton <houghton.brent@gmail.com>
2018-01-02 22:22:53 -0800

    Complete lesson 1.2 Hello Triangle from the learnopengl.com series

--- Learn OpenGL/src/main.rs
+++ Learn OpenGL/src/main.rs
@@ -5,8 +5,10 @@ extern crate tobj;
 
 #[cfg(feature = "chapter-1")]
 mod _1_1_hello_window;
+mod _1_2_hello_triangle;
 #[cfg(feature = "chapter-1")]
 use _1_1_hello_window::*;
+use _1_2_hello_triangle::*;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
@@ -18,6 +20,7 @@ fn main() {
 
     match tutorial_id.as_str() {
         #[cfg(feature = "chapter-1")] "1.1" => main_1_1(),
+        #[cfg(feature = "chapter-1")] "1.2" => main_1_2(),
 
         _     => println!("Unknown tutorial id")
     }
