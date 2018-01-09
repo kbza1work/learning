afc78fb53520fa01dcc97aebdcbd715dd774bf8f
Brent Houghton <houghton.brent@gmail.com>
2018-01-09 12:22:06 -0800

    Complete lesson 4 - Textures from learnopengl.com

---
+++ Learn OpenGL/src/_1_4_textures/mod.rs
@@ -0,0 +1,3 @@
+pub mod _1_4_textures;
+
+pub use self::_1_4_textures::main_1_4;
--- Learn OpenGL/src/main.rs
+++ Learn OpenGL/src/main.rs
@@ -3,16 +3,19 @@ extern crate image;
 extern crate cgmath;
 extern crate tobj;
 
+#[macro_use]
 mod common;
 
 #[cfg(feature = "chapter-1")]
 mod _1_1_hello_window;
 mod _1_2_hello_triangle;
 mod _1_3_shaders;
+mod _1_4_textures;
 #[cfg(feature = "chapter-1")]
 use _1_1_hello_window::*;
 use _1_2_hello_triangle::*;
 use _1_3_shaders::*;
+use _1_4_textures::*;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
@@ -26,6 +29,7 @@ fn main() {
         #[cfg(feature = "chapter-1")] "1.1" => main_1_1(),
         #[cfg(feature = "chapter-1")] "1.2" => main_1_2(),
         #[cfg(feature = "chapter-1")] "1.3" => main_1_3(),
+        #[cfg(feature = "chapter-1")] "1.4" => main_1_4(),
 
         _ => println!("Unknown tutorial id"),
     }
